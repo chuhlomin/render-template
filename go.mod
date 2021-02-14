28d1cac5a61c5a8b8a48a98a82cabdfc98c44bdc
Konstantin Chukhlomin <mail@chuhlomin.com>
2021-02-13 21:02:13 -0500

    ✨ Parse vars YAML

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/chuhlomin/kubectl-apply
+module github.com/chuhlomin/kat
 
 go 1.15
 
-require github.com/caarlos0/env/v6 v6.5.0
+require (
+	github.com/caarlos0/env/v6 v6.5.0
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
+)
--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"reflect"
 	"time"
 
 	"github.com/caarlos0/env/v6"
+	"gopkg.in/yaml.v3"
 )
 
+type vars map[string]interface{}
+
 type config struct {
-	Image              string                 `env:"INPUT_IMAGE,required"`
-	Template           string                 `env:"INPUT_TEMPLATE" envDefault:".kube.yml"`
-	SkipTemplate       bool                   `env:"INPUT_SKIP_TEMPLATE" envDefault:"false"`
-	SecretTemplate     string                 `env:"INPUT_SECRET_TEMPLATE" envDefault:".kube.sec.yml"`
-	SkipSecretTemplate bool                   `env:"INPUT_SKIP_SECRET_TEMPLATE" envDefault:"false"`
-	WaitDeployments    []string               `env:"INPUT_WAIT_DEPLOYMENTS" envDefault:""`
-	WaitDuration       time.Duration          `env:"INPUT_WAIT_DURATION" envDefault:"0"`
-	Vars               map[string]interface{} `env:"INPUT_VARS" envDefault:""`
+	Image              string        `env:"INPUT_IMAGE,required"`
+	Template           string        `env:"INPUT_TEMPLATE" envDefault:".kube.yml"`
+	SkipTemplate       bool          `env:"INPUT_SKIP_TEMPLATE" envDefault:"false"`
+	SecretTemplate     string        `env:"INPUT_SECRET_TEMPLATE" envDefault:".kube.sec.yml"`
+	SkipSecretTemplate bool          `env:"INPUT_SKIP_SECRET_TEMPLATE" envDefault:"false"`
+	WaitDeployments    []string      `env:"INPUT_WAIT_DEPLOYMENTS" envDefault:""`
+	WaitDuration       time.Duration `env:"INPUT_WAIT_DURATION" envDefault:"0"`
+	Vars               vars          `env:"INPUT_VARS" envDefault:""`
 }
 
 func main() {
@@ -30,11 +35,19 @@ func main() {
 
 func run() error {
 	var c config
-	if err := env.Parse(&c); err != nil {
+	parsers := map[reflect.Type]env.ParserFunc{
+		reflect.TypeOf(vars{}): func(v string) (interface{}, error) {
+			m := map[string]interface{}{}
+			err := yaml.Unmarshal([]byte(v), &m)
+			if err != nil {
+				return nil, fmt.Errorf("unable to parse Vars: %v", err)
+			}
+			return m, nil
+		},
+	}
+	if err := env.ParseWithFuncs(&c, parsers); err != nil {
 		return err
 	}
-
-	log.Printf("Config: %#v", c)
 
 	return nil
 }
